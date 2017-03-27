1d9b7a5a92c86e2bb559df178a3062b27237857e
mac ryndziewicz <fuji.two6@gmail.com>
2017-03-27 12:31:13 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+﻿// plik scripts.js
+
+var a = prompt('Podaj długość podstawy trójkąta');
+var h = prompt('Podaj wysokość trójkąta');
+var triangleArea = a*h/2;
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
